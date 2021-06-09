99d7d688e2773015e1db59a502788232c5f299a0
aviralverma-8877 <support@iot-connect.in>
2021-06-09 20:26:46 +0530

    Sensor Fix

--- src/global_var_two.cpp
+++ src/global_var_two.cpp
@@ -14,5 +14,5 @@ bool MQTTStatus = false;
 String chipid = String(ESP.getChipId());  //Fetching ESP device ID.
 String outtopic = chipid+"/RESPONSE";     //MQTT Topic for sending data from ESP.
 String intopic = chipid+"/COMMAND";
-String espsensor = "/ESP_SENSOR";         //MQTT Topic for sending sensor status data.
+String espsensor = "ESP_SENSOR";         //MQTT Topic for sending sensor status data.
 /*--------------MQTT Configration---------------------------*/
