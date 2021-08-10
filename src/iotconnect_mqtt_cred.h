723d6b7c31d18522a9cb95dbb24a3c4fc8cb3cf7
Aviral Verma <Aviral.Verma@wdc.com>
2021-08-10 13:54:02 +0530

    Fix

--- src/iotconnect_mqtt_cred.h
+++ src/iotconnect_mqtt_cred.h
@@ -1,16 +1,19 @@
+#ifndef iotconnect_mqtt
+    #define iotconnect_mqtt
 ////////////////////////Default MQTT Config//////////////////////////////////////////
 
-    #ifndef MQTT_HOST                                   //MQTT Server address
-        #define MQTT_HOST "mqtt.iot-connect.in"
-    #endif
-    #ifndef MQTT_PORT                                   //MQTT Server port
-        #define MQTT_PORT 1883
-    #endif
-    #ifndef MQTT_UNAME                                  //MQTT Username
-        #define MQTT_UNAME "iotconnect"
-    #endif
-    #ifndef MQTT_PASS                                   //MQTT Password
-        #define MQTT_PASS "iot-12345"
-    #endif
+        #ifndef MQTT_HOST                                   //MQTT Server address
+            #define MQTT_HOST "mqtt.iot-connect.in"
+        #endif
+        #ifndef MQTT_PORT                                   //MQTT Server port
+            #define MQTT_PORT 1883
+        #endif
+        #ifndef MQTT_UNAME                                  //MQTT Username
+            #define MQTT_UNAME "iotconnect"
+        #endif
+        #ifndef MQTT_PASS                                   //MQTT Password
+            #define MQTT_PASS "iot-12345"
+        #endif
 
 ////////////////////////Default MQTT Config//////////////////////////////////////////
+#endif
