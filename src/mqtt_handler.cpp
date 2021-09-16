78b06cb3a1416c3ce5703c0b7bc7ef600cf1f7da
Aviral Verma <sons2avitush@gmail.com>
2021-09-16 10:41:46 +0530

    revert

--- src/mqtt_handler.cpp
+++ src/mqtt_handler.cpp
@@ -19,6 +19,7 @@ void onMqttConnect(bool sessionPresent) {
   }
   MQTTStatus = true;
   reconnect_mqtt = false;
+  TickerForconnectToMqtt.detach();
   if (SPIFFS.exists("/mqtt_topics.json")) 
   {
     StaticJsonDocument<1000> doc;
@@ -51,6 +52,8 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
     reconnect_mqtt = true;
     mqtt.disconnect();
     mqtt.setCleanSession(true);
+    TickerForconnectToMqtt.detach();
+    TickerForconnectToMqtt.attach(5, setup_mqtt);
   }
   serialDisplay("onMqttDisconnect","MQTT is disconnected.");
 }
